a2c3eacfa733e838c2345f608465d23a11b17cce
Andrew Chuang <andrewjschuang@gmail.com>
2018-07-25 18:23:55 -0300

    Video capture running

--- video_capture.py
+++ video_capture.py
@@ -1,7 +1,10 @@
+import datetime
+from PIL import Image
+from pathlib import Path
+
 import face_recognition
 import cv2
 import fr_encodings
-from pathlib import Path
 
 # This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
 # other example, but it includes some basic performance tweaks to make things run a lot faster:
@@ -15,9 +18,7 @@ from pathlib import Path
 # Get a reference to webcam #0 (the default one)
 video_capture = cv2.VideoCapture(0)
 
-# andrew_image = face_recognition.load_image_file("/home/andrewjschuang/Pictures/icloud/download.png")
-# andrew_face_encoding = face_recognition.face_encodings(andrew_image)[0]
-
+# gets all saved encodings
 known_face_encodings = []
 known_face_names = []
 pathlist = Path('encodings/').glob('**/*.pk')
@@ -27,11 +28,12 @@ for path in pathlist:
     known_face_names.append(fr_encodings.load(path_in_str)['name'])
 
 # Initialize some variables
+found = []
 face_locations = []
 face_encodings = []
-face_names = []
 process_this_frame = True
 
+# run forever until Ctrl+C
 while True:
     # Grab a single frame of video
     ret, frame = video_capture.read()
@@ -46,47 +48,32 @@ while True:
     if process_this_frame:
         # Find all the faces and face encodings in the current frame of video
         face_locations = face_recognition.face_locations(rgb_small_frame)
+
+        # only this takes long
         face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
-        face_names = []
+        # checks for all faces
         for face_encoding in face_encodings:
             # See if the face is a match for the known face(s)
-            matches = face_recognition.compare_faces(known_face_encodings, face_encoding, tolerance=0.45)
-
-            name = "Unknown"
+            matches = face_recognition.compare_faces(known_face_encodings, face_encoding, tolerance=0.4)
 
             # If a match was found in known_face_encodings, just use the first one.
             if True in matches:
                 first_match_index = matches.index(True)
                 name = known_face_names[first_match_index]
 
-            face_names.append(name)
+                # don't repeat for found faces
+                if name in found:
+                    continue
+                found.append(name)
 
+                # save info
+                timestamp = datetime.datetime.now().strftime("%c")
+                filename = name + ' - ' + timestamp + '.png'
+                pil_image = Image.fromarray(frame)
+                pil_image.save('./found/' + filename)
+                with open('./found/out.txt', 'a') as f:
+                    f.write(str({'name': name, 'ts': timestamp, 'image': filename}) + '\n')
+
+    # process every other frame
     process_this_frame = not process_this_frame
-
-    # Display the results
-    for (top, right, bottom, left), name in zip(face_locations, face_names):
-        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-        top *= 4
-        right *= 4
-        bottom *= 4
-        left *= 4
-
-        # Draw a box around the face
-        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-
-        # Draw a label with a name below the face
-        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-        font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
-
-    # Display the resulting image
-    cv2.imshow('Video', frame)
-
-    # Hit 'q' on the keyboard to quit!
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# Release handle to the webcam
-video_capture.release()
-cv2.destroyAllWindows()
