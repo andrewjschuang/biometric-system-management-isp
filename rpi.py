22f2e1175d1482ec2778a4f9c8f228c9d2ee6986
Andrew Chuang <andrewjschuang@gmail.com>
2018-11-04 09:51:37 -0200

    Enables user input url

--- rpi.py
+++ rpi.py
@@ -2,9 +2,15 @@ import cv2
 import requests
 import datetime
 import time
+import sys
 from PIL import Image
 
 video_capture = cv2.VideoCapture(0)
+
+if len(sys.argv) == 2:
+    url = sys.argv[1]
+else:
+    url = 'http://192.168.2.116'
 
 while True:
     ret, frame = video_capture.read()
@@ -12,5 +18,5 @@ while True:
         filename = datetime.datetime.now().isoformat() + '.jpg'
         cv2.imwrite(filename, frame)
         with open(filename, 'rb') as f:
-            requests.post('http://localhost:5000', files={ 'file' : f })
+            requests.post(url, files={ 'file' : f })
     time.sleep(1)
