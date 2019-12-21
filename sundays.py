4894f55e90a6b3a78a9719789b3cab2243edbf31
Andrew Chuang <andrewjschuang@gmail.com>
2019-12-21 16:50:13 -0300

    initialize calendar with sundays only

---
+++ sundays.py
@@ -0,0 +1,28 @@
+import calendar
+import datetime
+
+def get_sundays_from_month(month, month_index):
+    days = []
+
+    for week in month:
+        day = week[6]
+        if day is not 0:
+            s = '%s-%s' % (month_index, day)
+            days.append(s)
+
+    return days
+
+def get_sundays_from_year(year=datetime.datetime.now().year):
+    days = []
+    c = calendar.Calendar().yeardayscalendar(year)
+    month_index = 1
+
+    for something in c:
+        for month in something:
+            days += get_sundays_from_month(month, month_index)
+            month_index += 1
+
+    return days
+
+if __name__ == '__main__':
+    print(get_sundays_from_year())
