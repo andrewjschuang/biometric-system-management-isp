322be3d33da537d00a4ae9517f312bf3fef6f12d
Andrew Chuang <andrewjschuang@gmail.com>
2025-05-01 16:23:32 -0300

    add event name and dry run params

--- backend/entities/Event.py
+++ backend/entities/Event.py
@@ -1,5 +1,5 @@
 class Event:
-    def __init__(self, member_id, name, timestamp, face_distance, photo, encoding=None, confirmed=False):
+    def __init__(self, member_id, name, timestamp, face_distance, photo, encoding=None, confirmed=False, event_name=None):
         self.member_id = member_id
         self.name = name
         self.timestamp = timestamp
@@ -7,9 +7,10 @@ class Event:
         self.photo = photo
         self.encoding = encoding
         self.confirmed = confirmed
+        self.event_name = event_name
 
     def __str__(self):
-        return 'Event(member_id=%s, name=%s, timestamp=%s, face_distance=%s, photo=%s, confirmed=%s)' % (self.member_id, self.name, self.timestamp, self.face_distance, self.photo, self.confirmed)
+        return 'Event(member_id=%s, name=%s, timestamp=%s, face_distance=%s, photo=%s, confirmed=%s, event_name=%s)' % (self.member_id, self.name, self.timestamp, self.face_distance, self.photo, self.confirmed, self.event_name)
 
     def to_dict(self):
         return {
@@ -19,8 +20,9 @@ class Event:
             'face_distance': self.face_distance,
             'photo': self.photo,
             'confirmed': self.confirmed,
+            'event_name': self.event_name,
         }
 
     @staticmethod
     def from_dict(event):
-        return Event(event['member_id'], event['name'], event['timestamp'], event['face_distance'], event['photo'], event.get('encoding'), event.get('confirmed'))
+        return Event(event['member_id'], event['name'], event['timestamp'], event['face_distance'], event['photo'], event.get('encoding'), event.get('confirmed'), event.get('event_name'))
